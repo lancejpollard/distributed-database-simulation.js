98fcfe5090c3497135e7088107c034174d5cd3d2
Lance Pollard <lp@elk.fm>
2022-02-13 01:10:32 -0800

    more

--- host.js
+++ host.js
@@ -1,7 +1,20 @@
 
+const KNEX = require('knex')
 const http = require('http')
 
-const port = process.argv[2]
+const integer = parseInt(process.argv[2])
+const port = 1234 + integer
+const databaseUrl = `postgresql://localhost:5432/ddsjs_test_${integer + 1}_replica_1`
+const knex = KNEX({
+  client: 'pg',
+  connection: {
+    connectionString: databaseUrl,
+  },
+  pool: {
+    min: 2,
+    max: 10
+  }
+})
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
